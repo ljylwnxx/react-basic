e2760bfb305e555965f7340dc6300f9215772070
ljylwnxx <ljylwnxx@163.com>
2023-05-24 12:15:47 +0800

    feat:JSX列表渲染

---
+++ src/02.JSX列表渲染.jsx
@@ -0,0 +1,23 @@
+// 定义列表
+const ancientPoetry = [
+    { id: 1, name: '《岳阳楼记》', author: '陶渊明', dynasty: '东晋'},
+    { id: 2, name: '《赤壁赋》', author: '苏轼', dynasty:'宋' },
+    { id: 3, name: '《将进酒》', author: '李白', dynasty:'唐' }
+  ]
+  
+  function App() {
+    return (
+      <div className="App">
+        <ul>
+          {
+           ancientPoetry.map(
+            item => <li>{item.name}-{item.author}-{item.dynasty}</li>
+            )
+          }
+        </ul>
+      </div>
+    )
+  }
+  
+  export default App
+  
