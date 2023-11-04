a7c251d2b107ff08f6c3588db43ad00860766314
ljylwnxx <ljylwnxx@163.com>
2023-11-04 16:17:32 +0800

    feat:获取事件对象

--- src/components/React组件基础/事件绑定/02.获取事件对象.jsx
+++ src/components/React组件基础/事件绑定/02.获取事件对象.jsx
@@ -1,8 +1,7 @@
-
 import React from 'react'
 
 // 函数组件的创建
-function Introduction () {
+function Introduction() {
   // 函数组件回调函数
   const functionHandleClick = (e) => {
     e.preventDefault()
@@ -27,18 +26,21 @@ class IntroductionComponet extends React.Component {
   componentHandleClick = (e, msg) => {
     console.log('类组件被点击了', e, msg)
   }
-  render () {
+  render() {
     return (
       <div>
         <h1>大家好，我叫wnxx</h1>
         <h1>我今年3岁了</h1>
-        <button onClick={(e) => this.componentHandleClick(e, '我是一只可爱的动物')}>类组件按钮</button>
+        <button
+          onClick={(e) => this.componentHandleClick(e, '我是一只可爱的动物')}>
+          类组件按钮
+        </button>
       </div>
     )
   }
 }
 
-function App () {
+function App() {
   return (
     <div className="App">
       {/*函数组件的渲染*/}
