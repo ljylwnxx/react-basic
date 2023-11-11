d5476b77e1e8c533dda2e9150d3ee3341411ecc2
ljylwnxx <ljylwnxx@163.com>
2023-11-11 18:06:16 +0800

    feat:受控组件

--- src/components/ReactComponentBasis/Form/ControlledFormComponent.jsx
+++ src/components/ReactComponentBasis/Form/ControlledFormComponent.jsx
@@ -8,16 +8,16 @@ class IntroductionComponent extends React.Component {
     hobbies: ['旅游', '唱歌'],
     animals: {
       name: 'wnxx',
-      age: 3
-    }
+      age: 3,
+    },
   }
   // 事件回调函数
   changeNameInput = (e) => {
     this.setState({
       animals: {
         ...this.state.animals,
-        name: e.target.value
-      }
+        name: e.target.value,
+      },
     })
   }
 
@@ -25,41 +25,61 @@ class IntroductionComponent extends React.Component {
     this.setState({
       animals: {
         ...this.state.animals,
-        age: e.target.value
-      }
+        age: e.target.value,
+      },
     })
   }
 
   changeWeightInput = (e) => {
     this.setState({
-      weight: e.target.value
+      weight: e.target.value,
     })
   }
 
   changeHobbiesInput = (e) => {
     this.setState({
-      hobbies: e.target.value
+      hobbies: e.target.value,
     })
   }
-  render () {
+  render() {
     // 使用状态
     return (
       <div>
-        <div className="name">大家好，我叫
-          <input type="text" value={this.state.animals.name} onChange={this.changeNameInput} />
+        <div className="name">
+          大家好，我叫
+          <input
+            type="text"
+            value={this.state.animals.name}
+            onChange={this.changeNameInput}
+          />
         </div>
-        <div className="age">我今年
-          <input type="text" value={this.state.animals.age} onChange={this.changeAgeInput} />
+        <div className="age">
+          我今年
+          <input
+            type="text"
+            value={this.state.animals.age}
+            onChange={this.changeAgeInput}
+          />
           岁了
         </div>
         <div className="weight">
-          <div>我的体重为：
-            <input type="text" value={this.state.weight} onChange={this.changeWeightInput} />
+          <div>
+            我的体重为：
+            <input
+              type="text"
+              value={this.state.weight}
+              onChange={this.changeWeightInput}
+            />
           </div>
         </div>
         <div className="hobbies">
-          <div>我的爱好包括：
-            <input type="text" value={this.state.hobbies} onChange={this.changeHobbiesInput} />
+          <div>
+            我的爱好包括：
+            <input
+              type="text"
+              value={this.state.hobbies}
+              onChange={this.changeHobbiesInput}
+            />
           </div>
         </div>
       </div>
@@ -67,7 +87,7 @@ class IntroductionComponent extends React.Component {
   }
 }
 
-function App () {
+function App() {
   return (
     <div className="App">
       {/*类组件的渲染*/}
