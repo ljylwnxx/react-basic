ac78cfe01de5d4ad3ea428a06cacefcad8b1145a
ljylwnxx <ljylwnxx@163.com>
2023-12-13 23:09:09 +0800

    chore:配置主题颜色

--- react-cashbook/src/pages/Layout/index.js
+++ react-cashbook/src/pages/Layout/index.js
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router-dom"
-
+import { Button } from 'antd'
 const Layout = () => {
   return (
     <div>
       <Outlet />
       layout
+      <Button color='primary'>ceshi</Button>
+      <div className='purple-theme'>
+        <Button color='primary'>ceshi</Button>
+      </div>
     </div>
   )
 }
