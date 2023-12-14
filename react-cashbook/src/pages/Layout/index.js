7f6e12473e6484bb404da8d1d991095c0c1bcd0e
ljylwnxx <ljylwnxx@163.com>
2023-12-14 16:24:52 +0800

    feat:tabbar切换功能实现

--- react-cashbook/src/pages/Layout/index.js
+++ react-cashbook/src/pages/Layout/index.js
@@ -1,8 +1,32 @@
-import { Outlet } from "react-router-dom"
-import { Button } from 'antd-mobile'
+import { Outlet, useNavigate } from "react-router-dom"
+import { TabBar } from 'antd-mobile'
 import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { getCashList } from '@/store/modules/cashStore'
+import {
+  AddCircleOutline,
+  BillOutline,
+  CalculatorOutline,
+} from "antd-mobile-icons"
+import './index.scss'
+
+const tabs = [
+  {
+    key: "/month",
+    title: "月度账单",
+    icon: <BillOutline />,
+  },
+  {
+    key: "/new",
+    title: "记账",
+    icon: <AddCircleOutline />,
+  },
+  {
+    key: "/year",
+    title: "年度账单",
+    icon: <CalculatorOutline />,
+  }
+]
 
 
 const Layout = () => {
@@ -10,13 +34,24 @@ const Layout = () => {
   useEffect(() => {
     dispatch(getCashList())
   }, [dispatch])
+  // 切换路由实现
+  const navigate = useNavigate()
+  const switchRoute = (path) => {
+    navigate(path)
+  }
   return (
-    <div>
-      <Outlet />
-      layout
-      <Button color='primary'>ceshi</Button>
-      <div className='purple-theme'>
-        <Button color='primary'>ceshi</Button>
+    <div className="layout">
+      {/* 路由出口 */}
+      <div className="container">
+        <Outlet />
+      </div>
+      {/* 页脚 */}
+      <div className="footer">
+        <TabBar onChange={switchRoute}>
+          {
+            tabs.map(item => (<TabBar.Item key={item.key} icon={item.icon} title={item.title} />))
+          }
+        </TabBar>
       </div>
     </div>
   )
